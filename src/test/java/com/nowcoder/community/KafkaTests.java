f89b3d7dafc736a0eb8a16e816cc0d1b1ad5dcda
zhaoyan <zhaoyan346a@sina.com>
2020-05-31 21:26:36 +0800

    整合kafka

---
+++ src/test/java/com/nowcoder/community/KafkaTests.java
@@ -0,0 +1,53 @@
+package com.nowcoder.community;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)//引入配置文件|配置类
+public class KafkaTests {
+
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
+    @Test
+    public void testKafka() {
+        kafkaProducer.sendMessage("test", "hello");
+        kafkaProducer.sendMessage("test", "world");
+        try {
+            Thread.sleep(1000*10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+@Component
+class KafkaProducer {
+    //注入spring创建的KafkaTemplate
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    public void sendMessage(String topic, String content) {
+        kafkaTemplate.send(topic, content);
+    }
+}
+
+@Component
+class KafkaConsumer {
+    //监听test 主题，有信息发过来了会自动装到ConsumerRecord中
+    @KafkaListener(topics = {"test"})
+    public void handleMessage(ConsumerRecord record) {
+        // .value()方法会输出发来的data
+        System.out.println(record.value());
+    }
+}
